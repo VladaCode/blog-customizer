02d4df2c339090e178f1d060716e4f0dcebdd29c
Vlada Sagermanova <sagermanovavl@gmail.com>
2025-03-21 15:23:00 +0300

    1. When you click on the "arrow", the sidebar with settings opens, when you click it again or click outside, the sidebar closes. 2. When you change the settings in the sidebar, they are not applied immediately. 3. After clicking "apply", the styles are applied to the article. 4. When you click "reset", the settings in the form are reset to the initial ones that were when the page was opened, and the styles are applied to the article. 5. The settings are set via CSS variables that are already in the styles and are set in the code to default values.

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,143 @@
+import { useEffect, useState, useRef } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
+import { RadioGroup } from 'src/ui/radio-group';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
+import {
+	fontFamilyOptions,
+	fontColors,
+	contentWidthArr,
+	fontSizeOptions,
+	defaultArticleState,
+	ArticleStateType,
+	OptionType,
+} from 'src/constants/articleProps';
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+//Пропсы для компанента ArticleParamsFormProps
+type TArticleParamsFormProps = {
+	onApply: (articleState: ArticleStateType) => void; // Колбэк при нажатии на кнопку "Применить"
+	onReset: () => void; // Колбэк при нажатии на кнопку "Сбросить"
+	initialState: ArticleStateType; // Новый пропс для начального состояния
+};
+
+export const ArticleParamsForm = ({
+	onApply,
+	onReset,
+	initialState, // Принимаем начальное состояние
+}: TArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState(false); // Состояние открытия/закрытия формы
+	const [formState, setFormState] = useState(initialState); // Инициализируем состояние формы переданным initialState
+	const asideRef = useRef<HTMLDivElement>(null); // Ref для сайдбара, чтобы отслеживать клики вне его области
+
+	// Обработчик открытия/закрытия сайдбара
+	const toggleSidebar = () => {
+		setIsOpen(!isOpen); //Установка нового значения isOpen на противоложное текущему
+	};
+
+	useEffect(() => {
+		// Ф-ция поверяет, был ли клик вне сайдбаре
+		const handleClickOutside = (e: MouseEvent) => {
+			// Если сайдбар существуе(asideRef.current) и клик был вне его области
+			if (asideRef.current && !asideRef.current.contains(e.target as Node)) {
+				//Закрытие сайдбара, устанавливая  setIsOpen в false
+				setIsOpen(false);
+			}
+		};
+
+		// Добавление обработчика события
+		document.addEventListener('mousedown', handleClickOutside);
+
+		// Удаляем обработчик при размонтировании компанента
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, []);
+
+	//Обработчик изменение значений в форме
+	const handleChange = (field: keyof ArticleStateType, value: OptionType) => {
+		setFormState((prevState) => ({
+			...prevState, //Копирование предыдущего состояние
+			[field]: value, // Обновление конкретного поля
+		}));
+	};
+
+	// Обработчик применения настроек
+	const handleApply = () => {
+		onApply(formState);
+	};
+
+	// Обработчик сброса настроек
+	const handlyReset = () => {
+		setFormState(defaultArticleState); // Сброс состояния формы к начальным значения
+		onReset();
+	};
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div ref={asideRef}>
+			<ArrowButton isOpen={isOpen} onClick={toggleSidebar} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				<form
+					className={styles.form}
+					onSubmit={(e) => {
+						e.preventDefault();
+						handleApply();
+					}}>
+					<Text
+						uppercase={true}
+						as={'h2'}
+						size={31}
+						weight={800}
+						align={'left'}
+						fontStyle={'normal'}
+						family={'open-sans'}>
+						Задайте параметры
+					</Text>
+					<Select
+						options={fontFamilyOptions}
+						selected={formState.fontFamilyOption}
+						onChange={(value) => handleChange('fontFamilyOption', value)}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						name='radio'
+						onChange={(value) => handleChange('fontSizeOption', value)}
+						title='Размер шрифта'
+					/>
+					<Select
+						options={fontColors}
+						selected={formState.fontColor}
+						onChange={(value) => handleChange('fontColor', value)}
+						title='Цвет шрифта'
+					/>
+					<Select
+						options={contentWidthArr}
+						selected={formState.contentWidth}
+						onChange={(value) => handleChange('contentWidth', value)}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handlyReset}
+						/>
+						<Button
+							title='Применить'
+							htmlType='submit'
+							type='apply'
+							onClick={handleApply}
+						/>
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -12,20 +15,32 @@ import styles from './styles/index.module.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
-const App = () => {
+export const App = () => {
+	const [articleStateApp, setArticleStateApp] = useState(defaultArticleState);
+
+	const handleApply = (newState: ArticleStateType) => {
+		console.log('Стили', newState);
+		setArticleStateApp(newState);
+	};
+
+	const handleReset = () => {
+		console.log('Брос стилей');
+		setArticleStateApp(defaultArticleState);
+	};
+
 	return (
 		<main
-			className={clsx(styles.main)}
+			className={styles.main}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articleStateApp.fontFamilyOption.value,
+					'--font-size': articleStateApp.fontSizeOption.value,
+					'--font-color': articleStateApp.fontColor.value,
+					'--container-width': articleStateApp.contentWidth.value,
+					'--bg-color': articleStateApp.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm onApply={handleApply} onReset={handleReset} initialState={articleStateApp}  />
 			<Article />
 		</main>
 	);
